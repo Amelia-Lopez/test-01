a4d3df115399bd639f9bf875c4618b7595d89a7b
Mario Lopez <Mario.Lopez@rackspace.com>
2016-09-07 19:27:53 -0500

    Added E-mail sender

---
+++ src/main/java/com/mariolopezjr/test/EmailSender.java
@@ -0,0 +1,8 @@
+package com.mariolopezjr.test;
+
+public class EmailSender {
+
+    public void sendEmail(String to, String from, String subject, String serverLogin, String body) {
+        // do nothing
+    }
+}
