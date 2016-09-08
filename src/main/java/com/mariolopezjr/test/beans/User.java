9ff48592ef234861a4a40ca012aa1731aaab767a
Mario Lopez <Mario.Lopez@rackspace.com>
2016-09-07 19:14:20 -0500

    Added User and UserDao

---
+++ src/main/java/com/mariolopezjr/test/beans/User.java
@@ -0,0 +1,9 @@
+package com.mariolopezjr.test.beans;
+
+public class User {
+    public int id;
+    public String name;
+    public int memberSince;
+    public int coachId;
+    public String workoutDetails;
+}
---
+++ src/main/java/com/mariolopezjr/test/db/UserDao.java
@@ -0,0 +1,13 @@
+package com.mariolopezjr.test.db;
+
+import com.mariolopezjr.test.beans.User;
+
+public class UserDao {
+    public User findUser(int id) {
+        return null;
+    }
+
+    public void persistUser(User user) {
+        // do nothing
+    }
+}
